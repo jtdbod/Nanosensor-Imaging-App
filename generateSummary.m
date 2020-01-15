e2881d433f7b06b7c439584ebf5e23778b73384d
Unknown <jackson.travis.do@gmail.com>
2020-01-14 16:56:21 -0800

    Remove non-significant traces from summary calculations

--- generateSummary.m
+++ generateSummary.m
@@ -10,16 +10,17 @@ Generate the following vales and STD (if applicable) and display in GUI and expo
 > Peak dF/F
 %}
 numRois = size(results.roiData,2);
-numSigs = length(find([results.fitData(:).significance]));
+sigIndex = find([results.fitData(:).significance]);
+numSigs = length(sigIndex);
 pctSig = numSigs./numRois;
 fitResults = reshape([results.fitData(:).fitResults].',4,[])';
 tau_on = nanmean(1./fitResults(:,3));
 tau_on_std = nanstd(1./fitResults(:,3));
 tau_off = nanmean(1./fitResults(:,2));
 tau_off_std = nanstd(1./fitResults(:,2));
-meanAuc = nanmean([results.roiData(:).auc]);
-meanAuc_std = nanstd([results.roiData(:).auc]);
-dFdetrend = reshape([results.roiData(:).dFdetrend].',length(results.roiData(1).dFdetrend),[])';
+meanAuc = nanmean([results.roiData(sigIndex).auc]);
+meanAuc_std = nanstd([results.roiData(sigIndex).auc]);
+dFdetrend = reshape([results.roiData(sigIndex).dFdetrend].',length(results.roiData(1).dFdetrend),[])';
 frameRate = results.imageStackInfo.frameRate;
 stimFrame = results.imageStackInfo.stimFrame;
 maxdFoverF = nanmean(max(dFdetrend(:,stimFrame:stimFrame+floor(3*frameRate)),[],2));
@@ -30,7 +31,7 @@ dataValues = {numRois numSigs pctSig tau_on tau_off...
 stdValues = {'-' '-' '-' tau_on_std tau_off_std meanAuc_std maxdFoverF_std};
 %}
 variableNames = {'ROI Total','Signif ROIs', 'Pct Sig ROIs',...
-    'Mean t On','Mean t Off','Mean AUC','Mean Peak dF/F'};
+    'Mean t On (s)','Mean t Off (s)','Mean AUC','Mean Peak dF/F'};
 dataFill = table({numRois;'-'},{numSigs;'-'},...
     {pctSig;'-'}, {tau_on;tau_on_std},...
     {tau_off;tau_off_std},{meanAuc;meanAuc_std},...
